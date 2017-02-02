b7862212b0ecc3cbff3e16174d314a75230dce6a
cas <cas@interglobal.vision>
2017-02-02 15:39:53 -0600

    add generate fingerprint on maincontainer

--- app/imports/containers/MainContainer.jsx
+++ app/imports/containers/MainContainer.jsx
@@ -1,19 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 
 import { compose } from 'react-komposer';
+import Fingerprint2 from 'fingerprintjs2';
 
 import { MainLayout } from '/imports/components/base/MainLayout.jsx';
 
 const composer = (props, onData) => {
 
-    /*
-  let user = Meteor.user() || null;
-  Session.set('User',user);
-
-  onData(null, { user });
-  */
-
-  onData(null, {});
+  new Fingerprint2().get((fingerprint) => {
+    Session.set('fingerprint', fingerprint);
+    onData(null, {});
+  });
 
 };
 
