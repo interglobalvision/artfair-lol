0e46af78092c9423dbd59b664f4ba9bd51680054
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-02-02 20:37:49 -0600

    make header sticky

--- app/imports/components/base/Header.jsx
+++ app/imports/components/base/Header.jsx
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
-
 import { PhotoInput } from '/imports/components/PhotoInput.jsx';
 
-export const Header = () => (
-  <header id='header' className='border-bottom-grey padding-top-small padding-bottom-small line-height-tighter'>
-    <div className='container'>
-      <nav id='header-nav' className='grid-row align-items-center text-align-center'>
-        <div className='grid-item item-s-2'>
-          <button className='button button-small font-size-small'>NEW</button>
+import StickyState from 'sticky-state';
+
+export class Header extends Component {
+  componentDidMount() {
+    new StickyState(document.querySelectorAll('.sticky'));
+  }
+
+  render() {
+    return (
+      <header id='header' className='sticky border-bottom-grey padding-top-small padding-bottom-small line-height-tighter'>
+        <div className='container'>
+          <nav id='header-nav' className='grid-row align-items-center text-align-center'>
+            <div className='grid-item item-s-2'>
+              <button className='button button-small font-size-small'>NEW</button>
+            </div>
+            <div className='grid-item item-s-8'>
+              <h1 className='font-sans font-size-large font-bold'>Artfair.lol</h1>
+            </div>
+            <div className='grid-item item-s-2'>
+              <PhotoInput />
+              <label id="add-photo-label" htmlFor="add-photo-input">
+                <img src="/icons/add_a_photo.svg" className="icon" />
+              </label>
+            </div>
+          </nav>
         </div>
-        <div className='grid-item item-s-8'>
-          <h1 className='font-sans font-size-large font-bold'>Artfair.lol</h1>
-        </div>
-        <div className='grid-item item-s-2'>
-          <PhotoInput />
-          <label id="add-photo-label" htmlFor="add-photo-input">
-            <img src="/icons/add_a_photo.svg" className="icon" />
-          </label>
-        </div>
-      </nav>
-    </div>
-  </header>
-);
+      </header>
+    );
+  }
+}
