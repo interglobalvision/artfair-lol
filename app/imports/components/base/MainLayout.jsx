25b294bd7d166ef578b1db84e6dbc20db0a91673
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-02-02 18:35:10 -0600

    add About to mainlayout

--- app/imports/components/base/MainLayout.jsx
+++ app/imports/components/base/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import { About } from '/imports/components/About.jsx';
 import { Header } from '/imports/components/base/Header.jsx';
 // import { Footer } from '/imports/components/footer.jsx';
 
 export const MainLayout = ({ content }) => (
   <div id="main-container">
+    <About />
     <Header />
     <div id="main-content">
       <div className="container">
