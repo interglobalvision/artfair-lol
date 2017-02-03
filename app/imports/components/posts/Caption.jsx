833682125a91cd783ed96e70b8fd904a7fb19045
cas <cas@interglobal.vision>
2017-02-03 14:50:02 -0600

    fix Caption to return null when caption === null

--- app/imports/components/posts/Caption.jsx
+++ app/imports/components/posts/Caption.jsx
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 import { autoParagraph } from '/imports/lib/misc.js';
 
 export const Caption = ({caption}) => {
+  if (caption === null) {
+    return null;
+  }
+
   return (
     <div className='grid-row post-caption'>
       <div className='grid-item item-s-12' dangerouslySetInnerHTML={autoParagraph(caption, 'caption margin-bottom-tiny')} ></div>
