6052152c726c6ec152aedaafc3edfcefdfdf3c51
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-02-03 18:59:10 -0600

    import commentsMethods to server

--- app/server/main.js
+++ app/server/main.js
@@ -5,6 +5,7 @@ import '/imports/startup/server/publications.js';
 // Methods
 //import '/imports/api/projectsMethods.js';
 import '/imports/api/photosMethods.js';
+import '/imports/api/commentsMethods.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
