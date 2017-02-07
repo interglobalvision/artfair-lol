d0940596360658a29215d99fba1ce348586a4c29
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-02-06 18:53:17 -0600

    update vote arrays on post document

--- app/imports/api/votesMethods.js
+++ app/imports/api/votesMethods.js
@@ -6,12 +6,42 @@ export const doVote = new ValidatedMethod({
     postId: {
       type: String,
     },
+    fingerprint: {
+      type: String,
+    },
     voteValue: {
       type: String,
     },
+    upVotes: {
+      type: [String],
+    },
+    downVotes: {
+      type: [String],
+    },
   }).validator(),
 
-  run({postId, voteValue}) {
-    console.log(voteValue);
+  run({postId, fingerprint, voteValue, upVotes, downVotes}) {
+    const upVoted = upVotes.indexOf( fingerprint ) > -1 ? true : false;
+    const downVoted = downVotes.indexOf( fingerprint ) > -1 ? true : false;
+
+    if (upVoted) {
+      Posts.update({_id: postId}, { $pull: { upVotes: fingerprint } } );
+
+      if (voteValue === 'down') {
+        Posts.update({_id: postId}, { $push: { downVotes: fingerprint } } );
+      }
+    } else if (downVoted){
+      Posts.update({_id: postId}, { $pull: { downVotes: fingerprint } } );
+
+      if (voteValue === 'up') {
+        Posts.update({_id: postId}, { $push: { upVotes: fingerprint } } );
+      }
+    } else {
+      if (voteValue === 'up') {
+        Posts.update({_id: postId}, { $push: { upVotes: fingerprint } } );
+      } else if (voteValue === 'down') {
+        Posts.update({_id: postId}, { $push: { downVotes: fingerprint } } );
+      }
+    }
   }
 });
