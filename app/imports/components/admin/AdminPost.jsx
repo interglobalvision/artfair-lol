8d95e7dbcbfbddf8b9be30a98c68bcc78abe2c3a
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-02-07 14:49:32 -0600

    add AdminPost, duplicate from FeedPost with added AdminActions

---
+++ app/imports/components/admin/AdminPost.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import { PostPhoto } from '/imports/components/posts/PostPhoto.jsx';
+import { Actions } from '/imports/components/posts/Actions.jsx';
+import { AdminActions } from '/imports/components/admin/AdminActions.jsx';
+import { Caption } from '/imports/components/posts/Caption.jsx';
+import { Comments } from '/imports/components/comments/Comments.jsx';
+import { TimeAgo } from '/imports/components/posts/TimeAgo.jsx';
+
+export class AdminPost extends Component {
+  render() {
+    return (
+      <div className='post-feed padding-bottom-mid'>
+
+        <PostPhoto photo={this.props.post.photo} />
+
+        <Actions upVotes={this.props.post.upVotes} downVotes={this.props.post.downVotes} postId={this.props.post._id} />
+
+        <AdminActions postId={this.props.post._id} />
+
+        <Caption caption={this.props.post.caption} />
+
+        <Comments comments={this.props.post.comments} limitComments={true} postId={this.props.post._id} />
+
+        <TimeAgo createdAt={this.props.post.createdAt} />
+
+      </div>
+    );
+  }
+};
