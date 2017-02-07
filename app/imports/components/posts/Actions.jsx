42e30a31c85c0e9082a200152765953c7c223d1e
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-02-06 23:41:48 -0600

    align center actions

--- app/imports/components/posts/Actions.jsx
+++ app/imports/components/posts/Actions.jsx
@@ -45,11 +45,11 @@ export class Actions extends Component {
     return (
       <div className='grid-row margin-bottom-small'>
         <div className='grid-item item-s-12'>
-          <div className='grid-row align-items-center line-height-tighter border-bottom-grey padding-top-tiny padding-bottom-tiny'>
+          <div className='grid-row align-items-center line-height-tighter border-bottom-grey padding-top-tiny padding-bottom-tiny text-align-center'>
             <div className='grid-item item-s-3 font-size-large font-bold num-votes'>{this.numVotes()}</div>
-            <div className='grid-item item-s-3 text-align-center'><img className='icon' src='/icons/thumb_up.svg' onClick={this.onVote} data-vote='up' data-voted={this.getUpvoted()} /></div>
-            <div className='grid-item item-s-3 margin-top-micro text-align-center'><img className='icon' src='/icons/thumb_down.svg' onClick={this.onVote} data-vote='down' data-voted={this.getDownvoted()} /></div>
-            <div className='grid-item item-s-3 margin-top-micro text-align-right'><a href={'/post/' + this.props.postId}><img className='icon' src='/icons/comment.svg' /></a></div>
+            <div className='grid-item item-s-3'><img className='icon' src='/icons/thumb_up.svg' onClick={this.onVote} data-vote='up' data-voted={this.getUpvoted()} /></div>
+            <div className='grid-item item-s-3 margin-top-micro'><img className='icon' src='/icons/thumb_down.svg' onClick={this.onVote} data-vote='down' data-voted={this.getDownvoted()} /></div>
+            <div className='grid-item item-s-3 margin-top-micro'><a href={'/post/' + this.props.postId}><img className='icon' src='/icons/comment.svg' /></a></div>
           </div>
         </div>
       </div>
