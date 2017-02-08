9339cac243828284ac104d74d42ebce5c0d42d54
cas <cas@interglobal.vision>
2017-02-07 18:08:36 -0600

    add resize image in hidden canvas, upload blob from canvas instead of image

--- app/imports/components/posts/NewPost.jsx
+++ app/imports/components/posts/NewPost.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import Hermite from '/imports/lib/hermite.js';
 import { distanceFrom } from '/imports/lib/geometry.js';
 
 import sanitizeHtml from 'sanitize-html';
@@ -32,11 +33,40 @@ export class NewPost extends Component {
     this.onInputChange = this.onInputChange.bind(this);
     this.onSubmitForm = this.onSubmitForm.bind(this);
     this.handleUpload = this.handleUpload.bind(this);
+    this.imageOnLoad = this.imageOnLoad.bind(this);
   }
 
   componentWillMount() {
-
     navigator.geolocation.getCurrentPosition(this.checkGeofence);
+  }
+
+  imageOnLoad(event) {
+    let img = new Image;
+    img.onload = this.resizeImage.bind(this);
+    img.src = this.state.photo;
+  }
+
+  resizeImage(event) {
+    let imageWidth = event.target.width;
+    let imageHeight = event.target.height;
+
+    let canvas = this.canvas;
+    let ctx = canvas.getContext('2d');
+
+    canvas.width = imageWidth;
+    canvas.height = imageHeight;
+
+    ctx.drawImage(event.target, 0, 0);
+
+    if (imageWidth > Meteor.settings.public.maxImageWidth) {
+      let ratio = Meteor.settings.public.maxImageWidth / imageWidth;
+
+      let hermite = new Hermite();
+
+      hermite.resample(canvas, imageWidth * ratio, imageHeight * ratio, true, function() {
+        console.log('ok');
+      });
+    }
 
   }
 
@@ -82,7 +112,7 @@ export class NewPost extends Component {
   }
 
   uploadFile() {
-    uploader = this.getSlingshotUploader();
+    const uploader = this.getSlingshotUploader();
     const photo = this.state.photo;
 
     this.setState({
@@ -112,11 +142,11 @@ export class NewPost extends Component {
     }, 100);
 
     // Use fetch to convert base64 to blob
-    fetch(photo)
-      .then( res => res.blob() )
-      .then( blob => {
-        uploader.send(blob, this.handleUpload);
-      });
+    let canvas = this.canvas;
+
+    canvas.toBlob( blob => {
+      uploader.send(blob, this.handleUpload);
+    });
   }
 
   handleUpload(error, url) {
@@ -184,7 +214,8 @@ export class NewPost extends Component {
 
         <div className="grid-row margin-bottom-small">
           <div className="grid-item item-s-12 no-gutter grid-row justify-center align-items-center">
-            <img className="post-image" src={this.state.photo} />
+            <img className="post-image" src={this.state.photo} onLoad={this.imageOnLoad} />
+            <canvas id="image-canvas" style={{display: "none"}} ref={canvas => this.canvas = canvas}></canvas>
           </div>
         </div>
         <form onSubmit={this.onSubmitForm}>
