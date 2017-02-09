caca11414e81ba393c87305eb74466b3e3ee2204
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-02-08 19:20:36 -0600

    add unavailable location notice

--- app/imports/components/posts/LocationNotice.jsx
+++ app/imports/components/posts/LocationNotice.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 
-export const LocationNotice = ({checking, approved, location}) => {
+export const LocationNotice = ({checking, approved, location, unavailable}) => {
   return (
-    <div className="grid-row padding-top-small padding-bottom-small justify-center">
+    <div className="grid-row padding-top-small padding-bottom-small justify-center text-align-center">
     {checking &&
         <span className="animation-phase"><img className='location-icon icon-small' src='/icons/location_searching.svg' />Finding your location...</span>
     }
     {!checking && approved &&
         <span><img className='location-icon icon-small' src='/icons/location_on.svg' />{location}</span>
     }
-    {!checking && !approved &&
+    {!checking && !approved && !unavailable &&
         <span><img className='location-icon icon-small' src='/icons/location_off.svg' />You must be at an art fair to post</span>
+    }
+    {!checking && !approved && unavailable &&
+        <span><img className='location-icon icon-small' src='/icons/location_off.svg' />We can't find your location.<br/>Please enable location for your browser<br/>and try again.</span>
     }
     </div>
   );
